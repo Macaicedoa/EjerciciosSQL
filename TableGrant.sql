6ff7531e35ee14d9352dcb02a65a1c13a13e5ab1
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-03-04 09:41:32 +0100

    Ejercicio revoke

--- TableGrant.sql
+++ TableGrant.sql
@@ -1,3 +1,5 @@
 --GRANT <command list> ON <table> TO <user>;
 GRANT SELECT,UPDATE ON Books TO 'martin@localhost';
 --users syntaxis: <user_name>@<ip-server>
+
+REVOKE UPDATE ON Books TO 'martin@localhost';
